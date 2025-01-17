9eea52de2a73cc587394ce11d6f86fbab33149d2
Maksym Baluta <maksimbaluta02@gmail.com.com>
2025-01-17 11:45:30 +0300

    chore: move Paths config to routerPaths folder

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,23 @@
 import s from "./Navbar.module.scss";
 import { NavLink } from "react-router-dom";
-import { Paths } from "../../shared/config";
+import { Paths } from "../../shared/routerPaths/config";
 export const Header = () => {
   return (
     <header className={s.header}>
       <div className={s.headerWrapper}>
-      <h1>
-        <NavLink className={s.logo} to={Paths.MAIN} >
-          FootballRes
-        </NavLink>
-      </h1>
+        <h1>
+          <NavLink className={s.logo} to={Paths.MAIN}>
+            FootballRes
+          </NavLink>
+        </h1>
 
-     
         <nav>
           <ul className={s.navList}>
             <li>
               <NavLink
                 className={({ isActive }) =>
                   isActive ? `${s.active}` : `${s.link}`
-                } 
+                }
                 to={Paths.RESULT}
               >
                 Результаты матчей
@@ -36,9 +35,7 @@ export const Header = () => {
             </li>
           </ul>
         </nav>
-
       </div>
-     
     </header>
   );
 };
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,16 +1,15 @@
-import { Route, Routes } from "react-router-dom"
-import { Paths } from "../shared/config"
-import { Main } from "../pages/Main/Main"
-import { ResultMatches } from "../pages/ResultMatches/ResultMatches"
-import { FutureMatches } from "../pages/FutureMatches/FutureMatches"
-
+import { Route, Routes } from "react-router-dom";
+import { Paths } from "../shared/routerPaths/config";
+import { Main } from "../pages/Main/Main";
+import { ResultMatches } from "../pages/ResultMatches/ResultMatches";
+import { FutureMatches } from "../pages/FutureMatches/FutureMatches";
 
 export const AppRouter = () => {
   return (
     <Routes>
-          <Route path={Paths.MAIN} element={<Main />} />
-          <Route path={Paths.RESULT} element={<ResultMatches />} />
-          <Route path={Paths.FUTURE} element={<FutureMatches />} />
-        </Routes>
-  )
-}
+      <Route path={Paths.MAIN} element={<Main />} />
+      <Route path={Paths.RESULT} element={<ResultMatches />} />
+      <Route path={Paths.FUTURE} element={<FutureMatches />} />
+    </Routes>
+  );
+};
